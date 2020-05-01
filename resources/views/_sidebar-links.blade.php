166dbaed5a9cf3371a7fbb666ed17e5a9943c403
Joze Perez <jozeperezp@gmail.com>
2020-05-01 08:32:49 -0400

    Create HTML template: Added raw sidebars to both sides of main section.

---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,32 @@
+<ul>
+    <li>
+        <a
+            class="font-bold text-lg mb-4 block"
+            href="/"
+        >Home</a>
+    </li>
+    <li>
+        <a
+            class="font-bold text-lg mb-4 block"
+            href="/explore"
+        >Explore</a>
+    </li>
+    <li>
+        <a
+            class="font-bold text-lg mb-4 block"
+            href="#"
+        >Notifications</a>
+    </li>
+    <li>
+        <a
+            class="font-bold text-lg mb-4 block"
+            href="#"
+        >Bookmarks</a>
+    </li>
+    <li>
+        <a
+            class="font-bold text-lg mb-4 block"
+            href="#"
+        >Lists</a>
+    </li>
+</ul>
