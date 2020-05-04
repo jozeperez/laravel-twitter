60e4839e4bbce4fef01fc399e0dbc6e06c7d8299
Joze Perez <jozeperezp@gmail.com>
2020-05-04 12:18:30 -0400

    Make interactive: Made timeline load tweets from Database. Connected form to controller allowing users to post new tweets.

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,17 +1,17 @@
                 <div class="flex p-4 border-b border-b-gray-400">
                     <div class="mr-4 flex-shrink-0">
                         <img
-                            src="https://i.pravatar.cc/50"
+                            src="{{ auth()->user()->avatar }}"
                             alt=""
                             class="rounded-full mr-2"
                         >
                     </div>
 
                     <div>
-                        <h5 class="font-bold mb-4">John Doe</h5>
+                        <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
 
                         <p class="text-sm">
-                            Troublin' mind jug, and I ain't comin' back Sally Ann steel driving crew traveling through this world of woe rye whiskey, cold wind blows going to town, sun would never shine, aces backed with eights and I ain't comin' back walking boss hopalong hoecake walking boss hang your head and cry in my sweet baby's arms they're gonna take Sadie to the burying ground ain't laid an egg since way last spring John Henry buried in the ground cluck old hen aces backed with eights rolling down the track they call The Cannonball steel driving crew in my sweet baby's arms!
+                            {{ $tweet->body }}
                         </p>
                     </div>
                 </div>
