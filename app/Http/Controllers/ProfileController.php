6a2c8e8cadd8f4ce6d9e26678da73be24a687429
joze PEREZ <jozeperezp@gmail.com>
2020-05-12 07:31:32 -0400

    Make interactive: created the edit user profile form. Users can now update basic profile information.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\User;
 
 class ProfileController extends Controller
@@ -15,5 +16,31 @@ class ProfileController extends Controller
     public function edit(User $user)
     {
         return view('profile.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $attributes = request()->validate([
+            'username' => [
+                'string',
+                'required',
+                'max:255',
+                'alpha_dash',
+                Rule::unique('users')->ignore($user)
+            ],
+            'name'     => ['string', 'required', 'max:255'],
+            'email'    => [
+                'string',
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($user)
+            ],
+            'password' => ['string', 'min:8', 'max:255', 'confirmed']
+        ]);
+
+        $user->update($attributes);
+
+        return redirect($user->path());
     }
 }
