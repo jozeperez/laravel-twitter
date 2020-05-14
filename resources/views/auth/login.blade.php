1f36daed4d20efa2055bb6d1e92108a5afa5075e
joze PEREZ <jozeperezp@gmail.com>
2020-05-14 07:21:11 -0400

    Clean up: Modified html of register form and adjusted styles. Touched loging form a little bit too.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,89 +1,88 @@
- <x-master>
+<x-master>
     <div class="container mx-auto flex justify-center">
         <div class="px-12 py-8 bg-gray-200 border border-gray-300 rounded-lg">
             <div class="col-md-8">
                 <div class="font-bold text-lg mb-4">{{ __('Login') }}</div>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="email"
+                        >
+                            Email
+                        </label>
 
-                        <div class="mb-6">
-                            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                                   for="email"
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="text"
+                               name="email"
+                               id="email"
+                               autocomplete="email"
+                               value="{{ old('email') }}"
+                               required
+                        >
+
+                        @error('email')
+                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="password"
+                        >
+                            Password
+                        </label>
+
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="password"
+                               name="password"
+                               id="password"
+                               autocomplete="current-password"
+                               value="{{ old('password') }}"
+                               required
+                        >
+
+                        @error('password')
+                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="mb-6">
+                        <div>
+                            <input class="mt-1"
+                               type="checkbox"
+                               name="remember"
+                               id="remember"
+                               {{ old('remember') ? 'checked' : '' }}
                             >
-                                Email
+
+                            <label class="uppercase font-bold text-xs text-gray-700"
+                                   for="remember"
+                            >
+                                Remember Me
                             </label>
-
-                            <input class="border border-gray-400 p-2 w-full"
-                                   type="text"
-                                   name="email"
-                                   id="email"
-                                   autocomplete="email"
-                                   value="{{ old('email') }}"
-                                   required
-                            >
-
-                            @error('email')
-                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                            @enderror
                         </div>
 
-                        <div class="mb-6">
-                            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                                   for="password"
-                            >
-                                Password
-                            </label>
+                        @error('remember')
+                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                            <input class="border border-gray-400 p-2 w-full"
-                                   type="password"
-                                   name="password"
-                                   id="password"
-                                   autocomplete="current-password"
-                                   value="{{ old('password') }}"
-                                   required
-                            >
+                    <div class="">
+                        <button type="submit"
+                                class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mr-2"
+                        >
+                            Submit
+                        </button>
 
-                            @error('password')
-                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                            @enderror
-                        </div>
-
-                        <div class="mb-6">
-                            <div>
-                                <input class="mt-1"
-                                   type="checkbox"
-                                   name="remember"
-                                   id="remember"
-                                   {{ old('remember') ? 'checked' : '' }}
-                                >
-
-                                <label class="uppercase font-bold text-xs text-gray-700"
-                                       for="remember"
-                                >
-                                    Remember Me
-                                </label>
-                            </div>
-
-                            @error('remember')
-                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                            @enderror
-                        </div>
-
-                        <div class="">
-                            <button type="submit"
-                                    class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mr-2"
-                            >
-                                Submit
-                            </button>
-
-                            <a href="{{ route('password.request') }}"
-                               class="text-xs text-gray-700"
-                            >
-                                Forgot Your Password?
-                            </a>
-                        </div>
-                    </form>
+                        <a href="{{ route('password.request') }}"
+                           class="text-xs text-gray-700"
+                        >
+                            Forgot Your Password?
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
